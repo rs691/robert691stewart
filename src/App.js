d2a8c5fbf61e0934cb2efee478b17adcc02e0a57
robert691stewart <robert691stewart@outlook.com>
2023-10-22 13:13:28 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     
        <h1>Hello there and welcome to my projects page</h1>
-      </header>
+   
     </div>
   );
 }
